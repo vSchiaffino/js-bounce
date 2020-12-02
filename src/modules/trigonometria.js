5bd0f7c63e605598ed22849a1cef6d1d3fdeec55
Valentin Schiaffino <valenschg@gmail.com>
2020-12-02 14:43:04 -0300

    adding few collission checkers

--- src/modules/trigonometria.js
+++ src/modules/trigonometria.js
@@ -68,4 +68,48 @@ export function circleRect(circle, rect){
 
     return d <= circle.r
 }
+
+export function linePoint(line, point){
+    let dx = Math.abs(line.p1.x - line.p2.x)
+    let dy = Math.abs(line.p1.y - line.p2.y)
+    let d = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2))
+
+    let d1x = Math.abs(line.p1.x - circle.x)
+    let d1y = Math.abs(line.p1.y - circle.y)
+    let d1 = Math.sqrt(Math.pow(d1x, 2) + Math.pow(d1y, 2))
+
+    let d2x = Math.abs(line.p2.x - circle.x)
+    let d2y = Math.abs(line.p2.y - circle.y)
+    let d2 = Math.sqrt(Math.pow(d2x, 2) + Math.pow(d2y, 2))
+
+    return d1 + d2 == d
+}
+
+export function pointCircle(point, circle) {
+    let dx = Math.abs(point.x - circle.x)
+    let dy = Math.abs(point.y - circle.y)
+    let d = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2))
+    return d <= r
+}
+
+export function lineCircle(line, circle) {
+    let inside1 = pointCircle(line.p1, circle)
+    let inside2 = pointCircle(line.p2, circle)
+    if ( inside1 || inside2)
+        return true;
+    let dx = line.p1.x - line.p2.x
+    let dy = line.p1.y - line.p2.y
+    let len = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2))
+
+    let dot = ( ((circle.x - line.p1.x)* (line.p2.x - line.p1.x)) + ((circle.y - line.p1.y)*(line.p2.y - line.p1.y)) ) / Math.pow(len, 2)
+    let closestX = line.p1.x + (dot * ( line.p2.x - line.p1.x ) )
+    let closestY = line.p1.y + (dot * ( line.p2.y - line.p1.y ) )
+    let onSegment = linePoint(line, {x: closestX, y: closestY})
+    if(!onSegment) return false
+
+    let distX = closestX - circle.x
+    let distY = closestY - circle.y
+    let distance = Math.sqrt(Math.pow(distX, 2) + Math.pow(distY, 2))
+    return distance <= circle.r
+}
 //#endregion
